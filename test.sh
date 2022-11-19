bd3a4aa78feab286ba5246830ab9c642b0c18080
Mete Balci <metebalci@gmail.com>
2022-11-19 22:42:09 +0100

    test fix

--- test.sh
+++ test.sh
@@ -2,14 +2,16 @@
 
 EXE=scripts/validate.py
 
+EXITCODE=0
+
 function do_test
 {
   q=$1
   rr=$2
 	rm -rf /tmp/digsec
 	mkdir /tmp/digsec
-	scripts/validate.py $q $rr /tmp/digsec
-  if [ $? -ne 0 ] 
+	scripts/validate.py $q $rr /tmp/digsec || EXITCODE=$?
+  if [ $EXITCODE -ne 0 ] 
   then
     exit 1
   fi
@@ -21,8 +23,8 @@ function do_ftest
   rr=$2
 	rm -rf /tmp/digsec
 	mkdir /tmp/digsec
-	scripts/validate.py $q $rr /tmp/digsec
-  if [ $? -eq 0 ] 
+	scripts/validate.py $q $rr /tmp/digsec || EXITCODE=$?
+  if [ $EXITCODE -eq 0 ] 
   then 
     exit 1
   fi
